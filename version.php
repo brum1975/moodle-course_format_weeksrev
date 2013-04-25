33806c8dbbc49fa6b7a2d1656286a6b0bf80a186
brum1975 <matthew.cannings@gmail.com>
2013-04-25 21:25:10 +0200

    Update version.php

--- version.php
+++ version.php
@@ -28,3 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2013042300;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2012062500;        // Requires this Moodle version.
 $plugin->component = 'format_weeksrev';    // Full name of the plugin (used for diagnostics).
+$plugin->release = '1.0 for Moodle 2.3 Only'
+$plugin->maturity  = MATURITY_RC;
